ae8ddb6646dbd396dd29632f05170841c4c6ee40
Vojtěch Grygar <vojtech.grygar@o2.cz>
2023-01-31 16:00:04 +0100

    Add AlzaClient

---
+++ Client/AlzaClient.cs
@@ -0,0 +1,27 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTest.Client
+{
+    internal class AlzaClient: IDisposable
+    {
+        readonly RestClient _client;
+        public AlzaClient()
+        {
+            var options = new RestClientOptions("https://webapi.alza.cz/api/career/v2");
+            _client = new RestClient(options);
+        }
+
+        public RestClient ClientInstance => _client;
+
+        public void Dispose()
+        {
+            _client?.Dispose();
+            GC.SuppressFinalize(this);
+        }
+    }
+}
