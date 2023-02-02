d9d4062fec8aaf8fbe33a53a1c5155150b911e1e
Vojtěch Grygar <vojtech.grygar@o2.cz>
2023-02-02 20:37:57 +0100

    Add test data for place of employment

---
+++ Test Data/IPlaceOfEmploymentAddress.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTest.Test_Data
+{
+    internal interface IPlaceOfEmploymentAddress
+    {
+        public string Name { get; }
+        
+        public string StreetName { get; }
+
+        public string StreetNumber { get; }
+
+        public string City { get; }
+
+        public string PostalCode { get; }
+
+        public double latitude { get; }
+
+        public double longitude { get; }
+
+    }
+}
---
+++ Test Data/PlaceOfEmploymentCZPraha.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTest.Test_Data
+{
+    internal class PlaceOfEmploymentCZPraha : IPlaceOfEmploymentAddress
+    {
+        //U Pergamenky 1522/2, Praha – Holešovice, 170 00
+        //50.1084772N, 14.4516217E
+        //50.108450000,14.452610000
+        /// <summary>
+        /// Data for job position 
+        /// </summary>
+        /// 
+        public PlaceOfEmploymentCZPraha() 
+        {
+
+        }
+
+        public string Name
+        {
+            get => "Hall office park";
+        }
+        public string StreetName
+        {
+            get => "U Pergamenky";
+        }
+        public string StreetNumber
+        {
+            get => "1522/2";
+        }
+        public string City
+        {
+            get => "Praha";
+        }
+        public string State
+        {
+            get => "Česká republika";
+        }
+        public string PostalCode
+        {
+            get => "17000";
+        }
+        public double latitude
+        {
+            get => 50.110;
+        }
+        public double longitude
+        {
+            get => 14.453;
+        }
+    }
+}
