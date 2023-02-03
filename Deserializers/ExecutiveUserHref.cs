12dbf8191f1b970fe126803218073decdeca546f
Vojtěch Grygar <vojtech.grygar@o2.cz>
2023-02-03 12:07:02 +0100

    Add ExecutiveUser

---
+++ Deserializers/ExecutiveUserHref.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace AlzaTest.Deserializers
+{
+    internal class ExecutiveUserHref
+    {
+        public JsonObject? executiveUser { get; set; }
+    }
+}
---
+++ Test Data/ExecutiveUser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlzaTest.Test_Data
+{
+    internal class ExecutiveUser : IUser
+    {
+        public ExecutiveUser() { }
+
+        public string Name
+        {
+            get => "Tomusko Ján";
+        }
+        public string Image
+        {
+            get => "image";
+        }
+        public string Description
+        {
+            get => "";
+        }
+    }
+}
