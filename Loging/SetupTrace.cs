228fc8426f3930ea0480ca760f35dd6a5b97bd63
Vojtěch Grygar <vojtech.grygar@o2.cz>
2023-02-04 01:50:01 +0100

    Fix project name

--- Loging/SetupTrace.cs
+++ Loging/SetupTrace.cs
@@ -22,13 +22,13 @@ public class SetupTrace
     }
     /// <summary>
     /// Get project directory absolute path 
-    /// e.g.: "C:\\Users\\vojte\\source\\repos\\TestProjectNunit\\"
+    /// e.g.: "C:\\Users\\vojte\\source\\repos\\AlzaTest\\"
     /// </summary>
     /// <param name="path"></param>
     private string ProjectDirectory()
     {
         List<string> pathSegments = TestContext.CurrentContext.TestDirectory.Split('\\').ToList<string>();
-        var projectDirPathIndex = pathSegments.FindIndex(x => x == "TestProjectNunit");
+        var projectDirPathIndex = pathSegments.FindIndex(x => x == "AlzaTest");
         string projectDirectory = string.Join("\\", pathSegments.Take<string>(projectDirPathIndex + 1));
         Console.WriteLine(projectDirectory);
         return projectDirectory;
